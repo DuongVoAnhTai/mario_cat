be7918ff7935a091758009ea5008acd61a17c8a7
Tai <theshy5893@gmail.com>
2023-10-19 13:39:04 +0700

    global file

--- mario_cat/game.h
+++ mario_cat/game.h
@@ -1,9 +1,5 @@
 #pragma once
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
+#include"global.cpp"
 
 class game {
 private:
