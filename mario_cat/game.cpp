243e460321a36ab8b774af159fcfa670c4490c8d
hoang12 <hoangluucoder1702@gmail.comgit config --global user.email hoangluucoder1702@gmail.com>
2023-11-20 20:22:57 +0700

    nhac nenvvvvvvvvb

--- mario_cat/game.cpp
+++ mario_cat/game.cpp
@@ -281,14 +281,6 @@ void Game::updateEnemies()
 }
 
 void Game::renderEnemies()
-{
-	this->updateEnemies();
-
-	//this->enemy->render(*this->window);
-}
-
-//Function
-void Game::pollEvent() {
 
 	while (this->window->pollEvent(this->ev)) 
 	{
@@ -312,6 +304,14 @@ void Game::music()
 }
 
 
+{
+	this->updateEnemies();
+
+	//this->enemy->render(*this->window);
+}
+
+//Function
+void Game::pollEvent() {
 void Game::update()
 {
 	this->pollEvent();
